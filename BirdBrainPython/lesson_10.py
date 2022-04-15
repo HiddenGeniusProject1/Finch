d4c364b4063d88b79df0ff4de8a34d48dcc18061
Trevor Walton <twalton@TheHiddsProject.attlocal.net>
2022-04-14 19:34:17 -0700

    New comments

--- BirdBrainPython/lesson_10.py
+++ BirdBrainPython/lesson_10.py
@@ -13,6 +13,30 @@ def exercise1():
         bird.setTurn('L',60,50)
     bird.stop()
 
-def exercise2()
+#Creates multiple circles in a pattern for the finch
+def exercise2():
+    def drawCircle():
+        bird.setMotors(0,50)
+        sleep(3)
+    for i in range(4):
+        drawCircle()
+        bird.setMove('F',15,50)
+    bird.stop()
+            
+def exercise3():
+    def lineTrack():
+        white = (bird.getLine('L') + bird.getLine('R'))/2
+        black = (bird.getLine('L') + bird.getLine('R'))/2
+        threshold =  (white+black)/2
+        if (bird.getLine('L') < threshold) or (bird.getLine('R') < threshold):
+            print("black")
+        else:
+            print("white")
+    while (bird.getDistance() > 20):
+        lineTrack()
+        bird.setMove('F',30,40)
+    bird.stop()
 
-exercise1()
+#exercise1()
+#exercise2()
+exercise3()
