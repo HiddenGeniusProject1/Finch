34cb0960fee811fe560ce7442b799426a3af3294
Trevor Walton <twalton@TheHiddsProject.attlocal.net>
2022-04-20 14:24:09 -0700

    Update

--- BirdBrainPython/lesson_10.py
+++ BirdBrainPython/lesson_10.py
@@ -23,6 +23,7 @@ def exercise2():
         bird.setMove('F',15,50)
     bird.stop()
             
+#Tracks the line sensors of what color it detects until the distance is less than 20
 def exercise3():
     def lineTrack():
         white = (bird.getLine('L') + bird.getLine('R'))/2
@@ -37,6 +38,26 @@ def exercise3():
         bird.setMove('F',30,40)
     bird.stop()
 
+def exercise4():
+    def wallFollowing():
+        while bird.getDistance() > 20:
+            bird.setMove('F',20,50)
+        bird.setTurn('R',90,50)
+        while bird.getDistance() > 10:
+            bird.setMove('F',10,50)
+        bird.setTurn('L',90,50)
+        while bird.getDistance() > 16:
+            bird.setMove('F',12,50)
+        bird.setTurn('L',90,50)
+        while bird.getDistance() > 10:
+            bird.setMove('F',5,50)
+        bird.setTurn('R',90,50)
+        bird.setMove('F',45,50)
+        bird.print("YOUDIDIT")
+    wallFollowing()
+
+
 #exercise1()
 #exercise2()
-exercise3()
+#exercise3()
+exercise4()
